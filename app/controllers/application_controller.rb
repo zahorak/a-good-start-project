28d6cd85e46296e22612736c6169dcf9b174f23b
Miro Zahorak <miro.zahorak@gmail.com>
2010-12-10 12:31:03 +0100

    changed order of filters to auth before locale is set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_locale
   before_filter :authenticate_user!
+  before_filter :set_locale
   def set_locale
     #@current_user=User.find(1)
     # if params[:locale] is nil then I18n.default_locale will be used
